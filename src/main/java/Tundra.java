07981f060e7aa18ec854b75e18907a4d43f3f9e3
zuoshihua <zuoshihua@proton.me>
2025-01-22 21:33:35 +0800

    Rename main class

---
+++ src/main/java/Tundra.java
@@ -0,0 +1,10 @@
+public class Tundra {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n"
+                + "Hello! I'm Tundra\n"
+                + "What can I do for you?\n\n"
+                + "Bye. Hope to see you again soon!\n\n"
+                + "____________________________________________________________\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
