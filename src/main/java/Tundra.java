55bcb0a14d0a6d3b5a3d57498e5f3272f0773f15
zuoshihua <zuoshihua@proton.me>
2025-01-23 11:35:26 +0800

    Added support for echo

--- src/main/java/Tundra.java
+++ src/main/java/Tundra.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Tundra {
+
     public static void main(String[] args) {
-        String greeting = "____________________________________________________________\n"
-                + "Hello! I'm Tundra\n"
-                + "What can I do for you?\n\n"
-                + "Bye. Hope to see you again soon!\n\n"
-                + "____________________________________________________________\n";
-        System.out.println("Hello from\n" + greeting);
+        Scanner sc = new Scanner(System.in);
+        String welcome = "\t____________________________________________________________\n"
+                + "\tHello! I'm Tundra\n"
+                + "\tWhat can I do for you?\n"
+                + "\t____________________________________________________________\n";
+        System.out.println(welcome);
+
+        String userInput = "";
+        while (true) {
+            userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            }
+            String response = "\t____________________________________________________________\n"
+                    + "\t" + userInput + "\n"
+                    + "\t____________________________________________________________\n";
+            System.out.println(response);
+        }
+
+        String goodbye = "\t____________________________________________________________\n"
+                + "\tBye. Hope to see you again soon!\n"
+                + "\t____________________________________________________________\n";
+        System.out.println(goodbye);
     }
 }
