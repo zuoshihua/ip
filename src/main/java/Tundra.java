34de41d08f3dbfd3b69e54ba20bfec6bd1ec94c5
zuoshihua <zuoshihua@proton.me>
2025-01-23 11:59:03 +0800

    Added support for create and retrieve operations

--- src/main/java/Tundra.java
+++ src/main/java/Tundra.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Tundra {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> db = new ArrayList<>();
         String welcome = "\t____________________________________________________________\n"
                 + "\tHello! I'm Tundra\n"
                 + "\tWhat can I do for you?\n"
@@ -13,13 +15,21 @@ public class Tundra {
         String userInput = "";
         while (true) {
             userInput = sc.nextLine();
-            if (userInput.equals("bye")) {
+            if (userInput.equalsIgnoreCase("list")) {
+                System.out.println("\t____________________________________________________________");
+                for (String s : db) {
+                    System.out.printf("\t%d. %s\n", db.indexOf(s) + 1, s);
+                }
+                System.out.println("\t____________________________________________________________");
+            } else if (userInput.equalsIgnoreCase("bye")) {
                 break;
+            } else {
+                db.add(userInput);
+                String response = "\t____________________________________________________________\n"
+                        + "\tadded: " + userInput + "\n"
+                        + "\t____________________________________________________________\n";
+                System.out.println(response);
             }
-            String response = "\t____________________________________________________________\n"
-                    + "\t" + userInput + "\n"
-                    + "\t____________________________________________________________\n";
-            System.out.println(response);
         }
 
         String goodbye = "\t____________________________________________________________\n"
