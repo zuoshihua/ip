795ad8b96a1bc2d4bb2261decdc6100bcb07b750
zuoshihua <zuoshihua@proton.me>
2025-01-23 15:01:12 +0800

    New fields from & to, custom toString for Event

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,2 +1,16 @@
-public class Event {
+public class Event extends Task {
+
+    private String from;
+    private String to;
+
+    public Event(String name, String from, String to) {
+        super(name);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[E]%s (from: %s to: %s)", super.toString(), from, to);
+    }
 }
