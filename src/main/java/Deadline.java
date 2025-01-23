015a2157df3ef2bedd05c69803f82780d8b06376
zuoshihua <zuoshihua@proton.me>
2025-01-23 14:59:46 +0800

    New field by and custom toString for Deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,2 +1,14 @@
-public class Deadline {
+public class Deadline extends Task {
+
+    private String by;
+
+    public Deadline(String name, String by) {
+        super(name);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[D]%s (by: %s)", super.toString(), by);
+    }
 }
