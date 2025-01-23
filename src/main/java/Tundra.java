cc380307dcefbc8572ca3593f3db52c90ef181c9
zuoshihua <zuoshihua@proton.me>
2025-01-23 15:03:59 +0800

    Add switch statement for different types of Task and their respective string parsing

--- src/main/java/Tundra.java
+++ src/main/java/Tundra.java
@@ -21,7 +21,7 @@ public class Tundra {
                 for (Task s : db) {
                     System.out.printf("\t%d.%s\n", db.indexOf(s) + 1, s);
                 }
-                System.out.println("\t____________________________________________________________");
+                System.out.println("\t____________________________________________________________\n");
             } else if (userInput.contains("mark")) {
                 String[] tokens = userInput.split(" ");
                 int i = Integer.parseInt(tokens[1]);
@@ -48,9 +48,31 @@ public class Tundra {
             } else if (userInput.equalsIgnoreCase("bye")) {
                 break;
             } else {
-                db.add(new Task(userInput));
+                String[] tokens = userInput.split(" ", 2);
+                Task t = null;
+                switch (tokens[0]) {
+                    case "todo":
+                        t = new Todo(tokens[1]);
+                        db.add(t);
+                        break;
+                    case "deadline":
+                        String[] dtokens = tokens[1].split(" /by ");
+                        t = new Deadline(dtokens[0], dtokens[1]);
+                        db.add(t);
+                        break;
+                    case "event":
+                        String name = tokens[1].split(" /from ")[0];
+                        String duration = tokens[1].split(" /from ")[1];
+                        String from = duration.split(" /to ")[0];
+                        String to = duration.split(" /to ")[1];
+                        t = new Event(name, from, to);
+                        db.add(t);
+                        break;
+                }
                 String response = "\t____________________________________________________________\n"
-                        + "\tadded: " + userInput + "\n"
+                        + "\tGot it. I've added this task:\n"
+                        + "\t " + t + "\n"
+                        + "\tNow you have " + db.size() + " tasks in the list.\n"
                         + "\t____________________________________________________________\n";
                 System.out.println(response);
             }
