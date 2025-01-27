5b49143f79385af1c31f59ea7ff4a4be47980133
zuoshihua <zuoshihua@proton.me>
2025-01-27 23:50:23 +0800

    Add Database class to handle persistence

---
+++ src/main/java/Database.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class Database {
+
+    private Database() {}
+
+    public static boolean load(File src, ArrayList<Task> dst) {
+        return false;
+    }
+
+    public static boolean store(ArrayList<Task> src, File dst) {
+        return false;
+    }
+}
