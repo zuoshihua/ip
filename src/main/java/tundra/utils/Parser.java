5b01267b85a1f191023219eb645fe216304d85d9
zuoshihua <zuoshihua@proton.me>
2025-01-31 20:38:02 +0800

    Update Parser to convert user input into command

--- src/main/java/tundra/utils/Parser.java
+++ src/main/java/tundra/utils/Parser.java
@@ -1,9 +1,34 @@
 package tundra.utils;
 
+import tundra.exceptions.TundraException;
+import tundra.views.Command;
+import tundra.views.CommandEnum;
+import tundra.views.HelpCommand;
+
 import java.time.format.DateTimeFormatter;
 
 public class Parser {
 
     public static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
     public static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy hh:mm a");
+
+    private Parser() {}
+
+    public static Command parse(String input) {
+        if (input.contains("|")) {
+            throw new TundraException("I'm sorry. You are not allowed to use the '|' symbol");
+        }
+        try {
+            String[] args = input.split(" ", 2);
+            String name = args[0].toUpperCase();
+            Command command = CommandEnum.valueOf(name).getCommand();
+            command.init(input);
+            return command;
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+            System.out.println(e.getClass().getSimpleName() + ": " + e.getMessage());
+            HelpCommand help = new HelpCommand();
+            help.init("");
+            return help;
+        }
+    }
 }
